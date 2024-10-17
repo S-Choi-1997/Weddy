32631bd2cd5cfa82d678ae75099317c728485394
이채연 <dldus0621@naver.com>
2024-10-17 17:45:59 +0900

    feat(api): api 로직 생성

--- frontend/src/apis/contractApi.ts
+++ frontend/src/apis/contractApi.ts
@@ -2,23 +2,23 @@ import axios from "axios"
 
 const BASE_URL = 'http://localhost:8080/api'
 
-//== 드레스 리스트 ==//
-export const dressList = async (): Promise<void> => {
+//== 계약서 정보 가져오기 ==//
+export const contractInfo = async (): Promise<void> => {
   const response = await axios({
     method: 'get',
-    url: `${BASE_URL}`,
+    url: `${BASE_URL}/`,
     headers: {
       Authorization: `Bearer `
     }
   });
-  
+
   console.log(response.data);
 }
 
-//== 메이크업 리스트 ==//
-export const makeupList = async (): Promise<void> => {
+//== 계약서 전자서명 ==//
+export const sign = async (): Promise<void> => {
   const response = await axios({
-    method: 'get',
+    method: 'patch',
     url: `${BASE_URL}/`,
     headers: {
       Authorization: `Bearer `
