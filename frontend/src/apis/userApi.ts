5f5d95d7511a9352153c44387957fd5285daa7eb
이채연 <dldus0621@naver.com>
2024-10-17 17:13:52 +0900

    feat(api): api 파일 생성 & env 파일 생성

---
+++ frontend/src/apis/userApi.ts
@@ -0,0 +1,14 @@
+//== 로그인 ==//
+export const login = () => {
+
+}
+
+//== 로그아웃 ==//
+export const logout = () => {
+
+}
+
+//== 마이페이지 정보 ==//
+export const myInfo = async (): Promise<void> => {
+
+}
