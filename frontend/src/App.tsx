889b4a63bc35ac6c112d3ffa10a7b260b276a099
이채연 <dldus0621@naver.com>
2024-10-21 11:35:52 +0900

    refactor: app.tsx 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import Navbar from './common/Navbar'
 import Main from './pages/MainPage'
 import Login from './pages/LoginPage'
 import './index.css'; 
-import Test from './test/Test'
-
 
 function App() {
 
@@ -17,7 +15,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<Login />} />
-          <Route path='/test' element={<Test />} />
         </Routes>
       </BrowserRouter>
       {location.pathname !== "/login" && (
