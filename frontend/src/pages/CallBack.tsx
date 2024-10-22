d36b93c71481b34ff39877b54836e2284dbc29ee
이채연 <dldus0621@naver.com>
2024-10-22 09:48:36 +0900

    feat(callback): callback 페이지
    callback 페이지에서 token 가져오기

--- frontend/src/pages/CallBack.tsx
+++ frontend/src/pages/CallBack.tsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { getToken } from "../apis/userApi";
+
 const CallBack = () => {
-  
-  return(
-    <>
-    </>
-  )
+  const navigate = useNavigate();
+  const params = new URLSearchParams(useLocation().search);
+  const userId = params.get('userId');
+
+  useEffect(() => {
+    const updateToken = async () => {
+      if (userId != undefined) {
+        await getToken(userId);
+        navigate('/');
+      };
+    };
+    updateToken();
+  }, [userId]);
+
+  return null;
 }
 
 export default CallBack;
