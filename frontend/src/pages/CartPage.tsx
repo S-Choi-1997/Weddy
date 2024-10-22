7a00fd3c9916bef5bc0bb82ddcd47e4614ce46a3
이채연 <dldus0621@naver.com>
2024-10-22 14:06:52 +0900

    fix: 주석처리

--- frontend/src/pages/CartPage.tsx
+++ frontend/src/pages/CartPage.tsx
@@ -3,11 +3,11 @@ import { getCartItems } from "../apis/productApi";
 
 const Cart = () => {
   //== 장바구니 목록 데이터 ==//
-  const { data:cartItem, isLoading } = useQuery('getCartItems', getCartItems);
+  // const { data:cartItem, isLoading } = useQuery('getCartItems', getCartItems);
 
-  if (isLoading) {
-    return <p>Loading</p>;
-  };
+  // if (isLoading) {
+  //   return <p>Loading</p>;
+  // };
   
   return (
     <div className="m-5">
