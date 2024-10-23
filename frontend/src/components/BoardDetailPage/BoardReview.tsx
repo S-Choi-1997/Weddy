4ae5920129130a32b849ff7642cc8d0e00fdaada
이채연 <dldus0621@naver.com>
2024-10-23 11:55:11 +0900

    feat(detail): 디테일 페이지 api

--- frontend/src/components/BoardDetailPage/BoardReview.tsx
+++ frontend/src/components/BoardDetailPage/BoardReview.tsx
@@ -1,3 +1,9 @@
+// import { Review } from "@/apis/product.type";
+
+// interface BoardReviewProp {
+//   reviewList: Review[]
+// };
+
 const BoardReview = () => {
   return (
     <div className="m-5">리뷰 페이지</div>
