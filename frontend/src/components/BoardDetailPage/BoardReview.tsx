b594a556930d53e6a1efa85a0f66da76025e8cec
이채연 <dldus0621@naver.com>
2024-10-23 11:33:16 +0900

    fix

--- frontend/src/components/BoardDetailPage/BoardReview.tsx
+++ frontend/src/components/BoardDetailPage/BoardReview.tsx
@@ -1,16 +1,4 @@
-// import { getReviewList } from "@/apis/productApi";
-// import { useQuery } from "react-query";
-// import { useParams } from "react-router-dom";
-
 const BoardReview = () => {
-  // const { productId } = useParams();
-
-  // const { data: reviewList } = useQuery(
-  //   ['getReviewList', productId],
-  //   () => getReviewList(productId),
-  //   {enabled:!productId}
-  // );
-
   return (
     <div className="m-5">리뷰 페이지</div>
   )
