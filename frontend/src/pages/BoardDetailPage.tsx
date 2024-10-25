cdc1cb6d542f3dc70898d0a1402f42059113e256
이채연 <dldus0621@naver.com>
2024-10-25 13:55:07 +0900

    feat(cart): 장바구니 담기 기능

--- frontend/src/pages/BoardDetailPage.tsx
+++ frontend/src/pages/BoardDetailPage.tsx
@@ -1,10 +1,11 @@
 // import { useQuery } from "react-query";
+import FooterButton from "@/components/BoardDetailPage/FooterButton";
 import BoardAsk from "../components/BoardDetailPage/BoardAsk";
 import BoardContent from "../components/BoardDetailPage/BoardContent";
 import BoardReview from "../components/BoardDetailPage/BoardReview";
 import { MainCarousel } from "../components/MainPage/MainCarousel";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "../components/ui/tabs";
-// import { addProductToCart, detailProduct, getPhone, getReviewList } from "@/apis/productApi";
+// import { detailProduct, getPhone, getReviewList } from "@/apis/productApi";
 // import { useParams } from "react-router-dom";
 
 const BoardDetail = () => {
@@ -16,6 +17,11 @@ const BoardDetail = () => {
   //   () => detailProduct(productId),
   //   {enabled: !!productId}
   // );
+
+  //== 장바구니 담기 ==//
+  const addToCart = async () => {
+    // await addProductToCart(productId);
+  };
 
   // //== 리뷰 리스트 ==//
   // const { data: reviewList } = useQuery(
@@ -30,11 +36,6 @@ const BoardDetail = () => {
   //   () => getPhone(productId),
   //   {enabled: !!productId}
   // );
-
-  // //== 장바구니 추가 ==//
-  // const addCart = async () => {
-  //   await addProductToCart(productId);
-  // };
 
   const dummyData = [
     '/dummy/test1.jpg',
@@ -74,6 +75,10 @@ const BoardDetail = () => {
         <TabsContent value="ask">
           <BoardAsk phone={phone}/>
         </TabsContent> */}
+
+        <div onClick={addToCart}>
+          <FooterButton />
+        </div>
       </Tabs>
     </div>
   )
