3edf431039c382530b8ecaae8885c9218dd3562f
이채연 <dldus0621@naver.com>
2024-10-29 14:32:43 +0900

    feat(schedule): 코드 수정

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,13 +1,11 @@
 import BestBox from "@/components/MainPage/BestBox";
 import { MainCarousel } from "../components/MainPage/MainCarousel";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 // import { useQuery } from "react-query";
 // import { getRankedProducts } from "@/apis/productApi";
 // import { Product } from "@/apis/product.type";
 
 const Main = () => {
-  const navigate = useNavigate();
-
   const dummyData = [
     '/dummy/test1.jpg',
     '/dummy/test2.jpg',
@@ -23,10 +21,6 @@ const Main = () => {
     '/main/main3.png',
   ];
 
-  const toDetail = () => {
-    navigate('/board/detail');
-  };
-
   // //== 베스트 ==//
   // const { data: getRankedProductList } = useQuery('getRankedProducts', getRankedProducts);
 
@@ -36,13 +30,13 @@ const Main = () => {
       <div className="grid grid-cols-2 gap-8 mt-10">
 
         {dummyData.map((src, index) => (
-          <div key={index} onClick={toDetail}>
+          <Link to={`/board/detail`} key={index}>
             <BestBox key={index} index={index+1} src={src} title="test" price={10000} />
-          </div>
+          </Link>
         ))}
 
         {/* {getRankedProductList?.map((product: Product, index) => (
-          <Link to={`/board/detail/${product.id}`}>
+          <Link to={`/board/detail/${product.id}`} key={index}>
             <BestBox key={product.id} index={index+1} src={product.productImage[0]} title={product.name} price={Number(product.price)} />
           </Link>
         ))} */}
--- frontend/src/pages/SchedulePage.tsx
+++ frontend/src/pages/SchedulePage.tsx
@@ -2,7 +2,7 @@ import CalenderBox from "../components/SchedulePage/CalenderBox";
 import ScheduleBox from "../components/SchedulePage/ScheduleBox";
 import PlusIcon from "../icons/PlusIcon";
 import { AlertDialogDemo } from "../components/SchedulePage/DrawerBox";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { useQuery } from "react-query";
 import { getSchedule } from "@/apis/scheduleApi";
 import { GetSchedule } from "@/apis/schedule.type";
@@ -10,7 +10,6 @@ import { GetSchedule } from "@/apis/schedule.type";
 const Schedule = () => {
   const [isOpen, setIsOpen] = useState(false);
   const [ selectedDate, setSelectedDate ] = useState<Date>(new Date());
-  const [ filteredList, setFilteredList ] = useState<GetSchedule[]>([]);
   console.log(selectedDate.toISOString());
 
   const { data: scheduleList } = useQuery(
@@ -27,17 +26,6 @@ const Schedule = () => {
     setSelectedDate(date);
   };
 
-  //== 해당 날짜의 스케줄 ==//
-  useEffect(() => {
-    const update = async () => {
-      const data = scheduleList?.filter((schedule: GetSchedule) => schedule.startDate <= selectedDate && schedule.endDate >= selectedDate);
-      if (data) {
-        setFilteredList(data);
-      }
-    };
-    update();
-  }, [selectedDate]);
-
   return (
     <div className="m-5 flex flex-col">
       <CalenderBox onDateChange={handleDateChange}/>
@@ -49,22 +37,21 @@ const Schedule = () => {
       </div>
 
       <div></div>
-      {/* <ScheduleBox type="studio" title="스튜디오 촬영" />
-      <ScheduleBox type="dress" title="드레스 피팅"/>
-      <ScheduleBox type="makeup" title="메이크업" />
-      <ScheduleBox type="etc" title="일정이 없습니다." /> */}
 
-      {filteredList.length <= 0 ? (
+      {!scheduleList || scheduleList?.length <= 0 ? (
         <ScheduleBox type="etc" title="일정이 없습니다." /> 
       ) : (
-        filteredList.map((schedule: GetSchedule) => {
+        scheduleList?.map((schedule: GetSchedule) => {
           switch (schedule.product.type) {
             case 'studio':
               return <ScheduleBox key={schedule.id} type="studio" title="스튜디오 촬영" />;
+            
             case 'dress':
               return <ScheduleBox key={schedule.id} type="dress" title="드레스 피팅" />;
+
             case 'makeup':
               return <ScheduleBox key={schedule.id} type="makeup" title="메이크업" />;
+
             default:
               return <ScheduleBox key={schedule.id} type="etc" title="기타 일정" />;
           }
