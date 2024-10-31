397bf0dfa3e967603a7d2ff3037d8ff7495b0dbb
이채연 <dldus0621@naver.com>
2024-10-31 15:31:44 +0900

    fix(login): 로그인 url 수정

--- frontend/src/api/userApi.ts
+++ frontend/src/api/userApi.ts
@@ -1,18 +1,18 @@
 import axios from "axios";
 import { userInformation } from "./user.type";
 
-const BASE_URL = "http://localhost:8080/api/users/token/super";
+const BASE_URL = "http://localhost:8080/api/users";
 
 //== 토큰 정보 ==//
 export const getToken = async (userId: string | null): Promise<void> => {
   const response = await axios({
     method: "get",
-    url: BASE_URL,
+    url: `${BASE_URL}//token/super`,
     params: {
       id: userId
     }
   });
-  console.log(response.data);
+  
   window.localStorage.setItem("token", response.data.accessToken);
 };
 
