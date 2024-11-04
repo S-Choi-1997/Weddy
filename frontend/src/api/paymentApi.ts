aefb93a6186e312285fba29e3366e68dabacccc9
이병수 <evil55@naver.com>
2024-11-05 04:05:18 +0900

    fix (payment) : 결제 url 수정

--- frontend/src/api/paymentApi.ts
+++ frontend/src/api/paymentApi.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 import * as PortOne from "@portone/browser-sdk/v2";
 import { ContractData } from "./contract.type";
 
-const BASE_URL = "http://localhost:8080/api/schedules";
+const BASE_URL = "http://localhost:8080/api/payments";
 const PORTONE_CHANNEL_KEY = import.meta.env.VITE_PORTONE_CHANNEL_KEY;
 const PORTONE_STORE_ID = import.meta.env.VITE_PORTONE_STORE_ID;
 
@@ -45,7 +45,7 @@ export const requestPayment = async (contractInfo: ContractData): Promise<void> 
 const sendPaymentSuccessToServer = async (contractInfo: ContractData): Promise<void> => {
   try {
     const response = await axios.post(
-      `${BASE_URL}/payment/success`,
+      `${BASE_URL}/success`,
       {
         productId: contractInfo.product.product_id,
         productName: contractInfo.product.product_name,
