5fee0d2a9ac679eb402910cb719a314266a8fdc0
이채연 <dldus0621@naver.com>
2024-11-05 10:58:43 +0900

    feat(contract): 계약 요청
    계약 요청 api 완료
    
    S11P31C203-185

--- frontend/src/api/contractApi.ts
+++ frontend/src/api/contractApi.ts
@@ -1,33 +1,9 @@
 import axios from "axios"
-import { ContractData, ContractProduct, SentContractType } from "./contract.type";
-import { Product } from "./product.type";
+import { ContractData, SentContractType } from "./contract.type";
 
 const BASE_URL = 'http://localhost:8080/api/contracts'
 
 const token = 'eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NSwidXNlck5hbWUiOiJb6rSR7KO8XzHrsJhfYzEwM1_snbTrs5HsiJhdIiwiY29kZSI6IkczNzFSTyIsImlhdCI6MTczMDQyNDQ1MSwiZXhwIjoxNzMzMDE2NDUxfQ.R7YFdmlN-IZkTeo0veuMA4W2eW_9-dXJJ-pGU8SRmPk'
-
-//== 계약서 생성 ==//
-export const createContract = async (data: Product): Promise<void> => {
-  const date = new Date().toISOString().slice(0, 10);
-  
-  const contractProduct: ContractProduct = {
-    product_id: data.id,
-    product_name: data.name,
-    product_content: data.content
-  };
-
-  const contract = {
-    "userId": "5",
-    "totalMount": data.price,
-    "companyName": data.vendorName,
-    "startDate": date,
-    "endDate": date,
-    "type": data.type,
-    "product": contractProduct
-  }
-
-  return requestContract([contract]);
-};
 
 //== 계약 요청 ==//
 export const requestContract = async (contract: SentContractType[]): Promise<void> => {
@@ -45,7 +21,7 @@ export const requestContract = async (contract: SentContractType[]): Promise<voi
     data: contract
   });
   console.log(response.data);
-}
+};
 
 //== 계약 리스트 ==//
 export const myContract = async (): Promise<ContractData[]> => {
@@ -77,18 +53,17 @@ export const contractInfo = async (contractId?: string): Promise<ContractData> =
   return response.data.data;
 };
 
-//== 계약서 전자서명 ==//
-export const signature = async (signature: string): Promise<void> => {
-  const response = await axios({
+//== 계약서 상태 변경 ==//
+export const changeStatus = async (contractId?: string): Promise<void> => {
+   const response = await axios({
     method: 'patch',
-    url: `${BASE_URL}/signature`,
+    url: `${BASE_URL}/${contractId}`,
     headers: {
       Authorization: `Bearer ${token}`
-    },
+    }
     // headers: {
-    //   Authorization: `Bearer ${sessionStorage.getItem("token")}`
-    // },
-    data: signature
-  });
-  console.log(response.data);
+    //   Authorization: sesssionstorage.getItem("token")
+    // }
+   });
+   console.log(response.data);
 };
