64481a42ab523173dbae1ee2b9d1a2db3b52ca39
이채연 <dldus0621@naver.com>
2024-11-06 09:11:14 +0900

    fix(api): token 코드 수정

--- frontend/src/api/productApi.ts
+++ frontend/src/api/productApi.ts
@@ -9,7 +9,7 @@ export const allProducts = async (): Promise<Product[]> => {
     method: 'get',
     url: BASE_URL,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   return response.data.data;
@@ -21,7 +21,7 @@ export const getRankedProducts = async (): Promise<Product[]> => {
     method: 'get',
     url: `${BASE_URL}/ranking`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   return response.data.data;
@@ -33,7 +33,7 @@ export const detailProduct = async (productId?: string): Promise<Product> => {
     method: 'get',
     url: `${BASE_URL}/${productId}`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   return response.data.data;
@@ -45,7 +45,7 @@ export const addProductToCart = async (productId?: string): Promise<void> => {
     method: 'post',
     url: `${BASE_URL}/${productId}/cart`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   console.log(response.data);
@@ -57,7 +57,7 @@ export const deleteFromCart = async (): Promise<void> => {
     method: 'delete',
     url: `${BASE_URL}/`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   console.log(response.data);
@@ -69,7 +69,7 @@ export const getCartItems = async (): Promise<Product[]> => {
     method: 'get',
     url: `${BASE_URL}/my`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   console.log(response.data);
@@ -82,7 +82,7 @@ export const getReviewList = async (productId?: string): Promise<ReviewData[]> =
     method: 'get',
     url: `${BASE_URL}/${productId}/review`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     }
   });
   return response.data.data;
@@ -94,7 +94,7 @@ export const submitReview = async (reviewData: ReviewData, productId?: string): 
     method: 'post',
     url: `${BASE_URL}/${productId}/review`,
     headers: {
-      Authorization: `Bearer `
+      Authorization: sessionStorage.getItem('token')
     },
     data: reviewData
   });
--- frontend/src/api/recommendApi.ts
+++ frontend/src/api/recommendApi.ts
@@ -1,18 +1,16 @@
-import axios from "axios"
+import axios from "axios";
 import { Product } from "./product.type";
 
-const BASE_URL = 'http://localhost:8000/api/recommends';
+const BASE_URL = "http://localhost:8000/api/recommends";
 
-export const aiRecommend = async (data: string): Promise<Product[]> => {
+export const aiRecommend = async (message: string): Promise<Product[]> => {
   const response = await axios({
-    method: 'get',
+    method: "get",
     url: BASE_URL,
     headers: {
-      Authorization: sessionStorage.getItem('token')
+      Authorization: sessionStorage.getItem("token"),
     },
-    data: {
-      message: data
-    }
+    data: message,
   });
   console.log(response.data);
   return response.data;
