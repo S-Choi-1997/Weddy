b36296da0425b3d5252b5e273e1aa95690b51165
이병수 <75319125+byeongsuLEE@users.noreply.github.com>
2024-11-06 18:01:57 +0900

    feat : schedule event 작성중

--- backend/schedule/src/main/java/com/ssafy/schedule/framework/kafkaadapter/ScheduleEventProducer.java
+++ backend/schedule/src/main/java/com/ssafy/schedule/framework/kafkaadapter/ScheduleEventProducer.java
@@ -1,9 +1,15 @@
 package com.ssafy.schedule.framework.kafkaadapter;
 
+import com.ssafy.schedule.domain.event.EventResult;
+import com.ssafy.schedule.domain.event.PaymentProductInfo;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
+
+import java.util.concurrent.CompletableFuture;
 
 @Service
 @RequiredArgsConstructor
@@ -11,8 +17,18 @@ import org.springframework.stereotype.Service;
 public class ScheduleEventProducer {
     @Value("${producers.topic1.name}")
     private String TOPIC;
+    private final KafkaTemplate<String, EventResult>  KafkaTemplate;
 
-
-
+    public void sendScheduleResultEvent(EventResult eventResult){
+        log.info("EventResult : "+eventResult.toString());
+        CompletableFuture<SendResult<String, EventResult>> send = KafkaTemplate.send(TOPIC, eventResult);
+        send.whenComplete((sendResult,ex)->{
+            if(ex!=null){
+                log.info("결제 이벤트 전달 실패."+ ex.getMessage());
+            }else{
+                log.info("결과 이벤트 전달 완료");
+            }
+        });
+    }
 
 }
