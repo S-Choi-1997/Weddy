fe33be269d021154c2658f66ee857a703c36dcfb
이병수 <evil55@naver.com>
2024-11-07 21:57:58 +0900

    feat(푸시 알림) : schedule 푸시알림 구현중

--- backend/schedule/src/main/java/com/ssafy/schedule/framework/kafkaadapter/ScheduleConsumers.java
+++ backend/schedule/src/main/java/com/ssafy/schedule/framework/kafkaadapter/ScheduleConsumers.java
@@ -11,6 +11,7 @@ import com.ssafy.schedule.framework.web.dto.output.ScheduleOutputDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -33,6 +34,8 @@ public class ScheduleConsumers {
 
     private final ScheduleEventProducer eventProducer;
 
+    private final RedisTemplate redisTemplate;
+
     @KafkaListener(topics = "${consumer.topic1.name}",groupId = "${consumer.groupid.name}")
     public void paymentProduct(ConsumerRecord<String, String> record) throws IOException {
 
@@ -46,9 +49,13 @@ public class ScheduleConsumers {
 
             log.info(paymentProductInfo.toString());
             //prododcut로 일정 생성
-            CreateScheduleInputDto dto = CreateScheduleInputDto.createScheduleInputDto(paymentProductInfo);
+            CreateScheduleInputDto scheduleInfo = CreateScheduleInputDto.createScheduleInputDto(paymentProductInfo);
             // UseCase를 통해 일정 생성
-            createScheduleUsecase.createSchedule(dto);
+            createScheduleUsecase.createSchedule(scheduleInfo);
+
+
+            //푸시알림전송
+            redisTemplate.opsForHash().put("schedule:"+ scheduleInfo.getStartDate(), scheduleInfo.getCode(),scheduleInfo.getContent(),scheduleInfo.getUserCoupleToken().getMyFcmToken());
 
             // 성공 시 이벤트 발생
             eventResult.updateIsSuccess(true);
