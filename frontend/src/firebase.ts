9e663dab5db83c027a754debdf90312391153bbe
이호영 <hoo4053@naver.com>
2024-11-08 14:48:59 +0900

    fix: 빌드 에러 해결

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -59,35 +59,4 @@ export const onMessageListener = (): Promise<MessagePayload> => {
   });
 };
 
-// 일정 데이터 타입 정의
-// interface Schedule {
-//   title: string;
-//   message: string;
-//   date: Date; // 알림을 보낼 날짜
-// }
-
-// Firestore에 일정 데이터를 저장하는 함수
-// export const saveSchedule = async (schedule: Schedule): Promise<void> => {
-//   try {
-//     // FCM 토큰 생성
-//     const fcmToken = await requestForToken();
-
-//     if (fcmToken) {
-//       // Firestore에 일정 데이터 저장
-//       await addDoc(collection(firestore, "schedules"), {
-//         ...schedule,
-//         fcmToken,  // 알림을 보낼 FCM 토큰 저장
-//         alertTime: schedule.date.getTime() - 24 * 60 * 60 * 1000,  // 하루 전 알림 시간 (밀리초로 변환)
-//         createdAt: new Date().toISOString()  // 생성 시간
-//       });
-//       console.log("일정이 성공적으로 저장되었습니다.");
-//     } else {
-//       console.warn("FCM 토큰 생성에 실패하여 일정 알림을 설정할 수 없습니다.");
-//     }
-//   } catch (err) {
-//     console.error("Firestore에 일정 데이터를 저장하는 중 오류가 발생했습니다:", err);
-//   }
-// };
-
-// messaging 객체 export
 export { messaging };
