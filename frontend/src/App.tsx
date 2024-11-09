0b00828b6e3448d364a14e86f594037c8da45395
이호영 <hoo4053@naver.com>
2024-11-09 14:26:32 +0900

    fix:addEventListener 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,42 +1,37 @@
 import { QueryClient, QueryClientProvider } from "react-query";
-import { BrowserRouter, Route, Routes, useLocation } from 'react-router-dom';
-import Footer from './common/Footer';
-import Navbar from './common/Navbar';
-import './index.css';
-import BoardDetail from './pages/BoardDetailPage';
-import Board from './pages/BoardPage';
-import CallBack from './pages/CallBack';
-import Cart from './pages/CartPage';
-import ContractList from './pages/ContractListPage';
-import Contract from './pages/ContractPage';
-import Login from './pages/LoginPage';
-import Main from './pages/MainPage';
-import Mypage from './pages/MyPage';
+import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./common/Footer";
+import Navbar from "./common/Navbar";
+import "./index.css";
+import BoardDetail from "./pages/BoardDetailPage";
+import Board from "./pages/BoardPage";
+import CallBack from "./pages/CallBack";
+import Cart from "./pages/CartPage";
+import ContractList from "./pages/ContractListPage";
+import Contract from "./pages/ContractPage";
+import Login from "./pages/LoginPage";
+import Main from "./pages/MainPage";
+import Mypage from "./pages/MyPage";
 import NFTLoading from "./pages/NFTLoadingPage";
 import PlannerList from "./pages/PlannerListPage";
-import Planner from './pages/PlannerPage';
-import Prompt from './pages/PromptPage';
+import Planner from "./pages/PlannerPage";
+import Prompt from "./pages/PromptPage";
 import RecommendLoading from "./pages/RecommendLoadingPage";
 import Review from "./pages/ReviewPage";
-import Schedule from './pages/SchedulePage';
-import Sketch from './pages/SketchPage';
+import Schedule from "./pages/SchedulePage";
+import Sketch from "./pages/SketchPage";
 import UserInfo from "./pages/UserInfoPage";
-import { useSetRecoilState } from 'recoil';
-import { firebaseTokenState } from './store/firebaseToken.ts';
-import { useEffect } from 'react';
-import { saveFcmToken } from "./api/userApi.ts";
-import { requestForToken, requestNotificationPermission } from './firebase.ts';
 import DressSketch from "./pages/DressSketchPage.tsx";
 import DressImg from "./pages/DressImgPage.tsx";
 
 function AppContent() {
   const location = useLocation();
-  const currentPath = location.pathname.split('/')[1];
-  const currentDetail = location.pathname.split('/')[2];
+  const currentPath = location.pathname.split("/")[1];
+  const currentDetail = location.pathname.split("/")[2];
 
   return (
     <>
-      {(currentPath !== "login") && (currentPath !== "userinfo") && <Navbar />}
+      {currentPath !== "login" && currentPath !== "userinfo" && <Navbar />}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/review/:productId" element={<Review />} />
@@ -59,65 +54,18 @@ function AppContent() {
         <Route path="/nft/loading" element={<NFTLoading />} />
         <Route path="/recommend/loading" element={<RecommendLoading />} />
       </Routes>
-      {(currentPath !== "login") && (currentPath !== "userinfo") && (currentDetail !== "detail") && <Footer />}
+      {currentPath !== "login" &&
+        currentPath !== "userinfo" &&
+        currentDetail !== "detail" && <Footer />}
     </>
   );
 }
 
 function App() {
   const queryClient = new QueryClient();
-  const setToken = useSetRecoilState(firebaseTokenState);
-  const userId = sessionStorage.getItem("userId");
-
-  useEffect(() => {
-    const registerServiceWorker = async () => {
-      if ('serviceWorker' in navigator) {
-        const registrations = await navigator.serviceWorker.getRegistrations();
-        const isRegistered = registrations.some((registration) =>
-          registration.active && registration.scope === '/firebase-messaging-sw.js'
-        );
-
-        if (!isRegistered) {
-          try {
-            await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-            console.log('Service Worker registered successfully');
-          } catch (err) {
-            console.error('Service Worker registration failed:', err);
-          }
-        } else {
-          console.log('Service Worker already registered');
-        }
-      }
-    };
-
-    registerServiceWorker();
-  }, []);
-
-  useEffect(() => {
-    const requestPermissionsAndToken = async () => {
-      if (userId) {
-        try {
-          await requestNotificationPermission();
-          const token = await requestForToken();
-          if (token) {
-            setToken(token);
-            saveFcmToken(token, userId);
-          } else {
-            console.warn("No token received");
-          }
-        } catch (error) {
-          console.error("Error requesting permissions or token:", error);
-        }
-      } else {
-        console.warn("User ID is null, skipping requestPermissionsAndToken");
-      }
-    };
-
-    requestPermissionsAndToken();
-  }, [setToken, userId]);
 
   return (
-    <div className='container'>
+    <div className="container">
       <QueryClientProvider client={queryClient}>
         <BrowserRouter>
           <AppContent />
--- frontend/src/pages/CallBack.tsx
+++ frontend/src/pages/CallBack.tsx
@@ -1,7 +1,10 @@
 import { getToken, getUserInfo } from "@/api/userApi";
+import { requestForToken, requestNotificationPermission } from "@/firebase";
+import { firebaseTokenState } from "@/store/firebaseToken";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useLocation, useNavigate } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
 
 const CallBack = () => {
   const navigate = useNavigate();
@@ -9,6 +12,10 @@ const CallBack = () => {
   const userId = params.get('id');
 
   const [ userInfoEnabled, setUserInfoEnabled ] = useState(false);
+
+
+  const setToken = useSetRecoilState(firebaseTokenState);
+  // const userId = sessionStorage.getItem("userId");
   
   //== 토큰 정보 ==//
   useQuery(
@@ -21,6 +28,54 @@ const CallBack = () => {
       }
     }
   );
+
+  useEffect(() => {
+    const registerServiceWorker = async () => {
+      if ('serviceWorker' in navigator) {
+        const registrations = await navigator.serviceWorker.getRegistrations();
+        const isRegistered = registrations.some((registration) =>
+          registration.active && registration.scope === '/firebase-messaging-sw.js'
+        );
+
+        if (!isRegistered) {
+          try {
+            await navigator.serviceWorker.register('/firebase-messaging-sw.js');
+            console.log('Service Worker registered successfully');
+          } catch (err) {
+            console.error('Service Worker registration failed:', err);
+          }
+        } else {
+          console.log('Service Worker already registered');
+        }
+      }
+    };
+
+    registerServiceWorker();
+  }, []);
+
+
+  useEffect(() => {
+    const requestPermissionsAndToken = async () => {
+      if (userId) {
+        try {
+          await requestNotificationPermission();
+          const token = await requestForToken();
+          if (token) {
+            setToken(token);
+            // saveFcmToken(token, userId);
+          } else {
+            console.warn("No token received");
+          }
+        } catch (error) {
+          console.error("Error requesting permissions or token:", error);
+        }
+      } else {
+        console.warn("User ID is null, skipping requestPermissionsAndToken");
+      }
+      
+    };
+      requestPermissionsAndToken();
+    }, [setToken, userId]);
 
   //== 회원 정보 ==//
   const { data: userInfo } = useQuery('getUserInfo', getUserInfo, {
