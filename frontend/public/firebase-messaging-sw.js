c4c2c311d966b26ea0b9dc54254f09ffea6ecc32
byeongsuLEE <75319125+byeongsuLEE@users.noreply.github.com>
2024-11-11 12:00:38 +0900

    feat(푸시)  : 푸시알림 수정

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -1,6 +1,10 @@
 // firebase-messaging-sw.js
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js"
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js"
+);
 
 firebase.initializeApp({
   apiKey: "AIzaSyDjRoo7pLucixUH8hPRF792FuUIOcwiK3k",
@@ -9,13 +13,13 @@ firebase.initializeApp({
   storageBucket: "weddy-69a91.firebasestorage.app",
   messagingSenderId: "194642239012",
   appId: "1:194642239012:web:b8db8875468161f4b042b1",
-  measurementId: "G-SYX7QVXCHF"
+  measurementId: "G-SYX7QVXCHF",
 });
 
 const messaging = firebase.messaging();
 
 // 백그라운드 메시지 수신 핸들러
-self.addEventListener('push', function (event) {
+self.addEventListener("push", function (event) {
   console.log("Push event received:", event);
 
   let data = { title: "기본 제목", body: "기본 내용" };
@@ -29,57 +33,58 @@ self.addEventListener('push', function (event) {
 
   const options = {
     body: data.body,
-    icon: "/default-icon.png"
+    icon: "/default-icon.png",
   };
 
   console.log("Attempting to show notification:", data.title, options);
-  // event.waitUntil(
-  //   self.registration.showNotification(data.title, options)
-  // );
+  event.waitUntil(self.registration.showNotification(data.title, options));
 });
 
 // PWA 캐싱 설정
 const CACHE_NAME = "weddy-cache-v1";
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/offline.html',
-  '/styles.css',
-  '/default-icon.png'
+  "/",
+  "/index.html",
+  "/offline.html",
+  "/styles.css",
+  "/default-icon.png",
 ];
 
-self.addEventListener('install', function(event) {
+self.addEventListener("install", function (event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log("Opened cache");
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // 서비스워커 활성화 및 오래된 캐시 삭제
-self.addEventListener('activate', function(event) {
+self.addEventListener("activate", function (event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.filter(function(cacheName) {
-          return cacheName !== CACHE_NAME;
-        }).map(function(cacheName) {
-          return caches.delete(cacheName);
-        })
+        cacheNames
+          .filter(function (cacheName) {
+            return cacheName !== CACHE_NAME;
+          })
+          .map(function (cacheName) {
+            return caches.delete(cacheName);
+          })
       );
     })
   );
 });
 
 // 네트워크 요청 가로채기 및 캐시 제공
-self.addEventListener('fetch', function(event) {
+self.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // 캐시된 응답이 있으면 반환하고, 없으면 네트워크 요청
-        return response || fetch(event.request).catch(() => caches.match('/offline.html'));
-      })
+    caches.match(event.request).then(function (response) {
+      // 캐시된 응답이 있으면 반환하고, 없으면 네트워크 요청
+      return (
+        response ||
+        fetch(event.request).catch(() => caches.match("/offline.html"))
+      );
+    })
   );
 });
--- frontend/src/pages/CallBack.tsx
+++ frontend/src/pages/CallBack.tsx
@@ -1,58 +1,53 @@
-import { getToken, getUserInfo } from "@/api/userApi";
+import { getToken, getUserInfo, saveFcmToken } from "@/api/userApi";
 import { requestForToken, requestNotificationPermission } from "@/firebase";
 import { firebaseTokenState } from "@/store/firebaseToken";
 import { useEffect, useState } from "react";
 import { useQuery } from "react-query";
 import { useLocation, useNavigate } from "react-router-dom";
 import { useSetRecoilState } from "recoil";
-
 const CallBack = () => {
   const navigate = useNavigate();
   const params = new URLSearchParams(useLocation().search);
-  const userId = params.get('id');
+  const userId = params.get("id");
 
-  const [ userInfoEnabled, setUserInfoEnabled ] = useState(false);
-
+  const [userInfoEnabled, setUserInfoEnabled] = useState(false);
 
   const setToken = useSetRecoilState(firebaseTokenState);
   // const userId = sessionStorage.getItem("userId");
-  
+
   //== 토큰 정보 ==//
-  useQuery(
-    ['getToken', userId],
-    () => getToken(userId ?? undefined),
-    {
-      enabled: !!userId,
-      onSuccess: () => {
-        setUserInfoEnabled(true);
-      }
-    }
-  );
+  useQuery(["getToken", userId], () => getToken(userId ?? undefined), {
+    enabled: !!userId,
+    onSuccess: () => {
+      setUserInfoEnabled(true);
+    },
+  });
 
   useEffect(() => {
     const registerServiceWorker = async () => {
-      if ('serviceWorker' in navigator) {
+      if ("serviceWorker" in navigator) {
         const registrations = await navigator.serviceWorker.getRegistrations();
-        const isRegistered = registrations.some((registration) =>
-          registration.active && registration.scope === '/firebase-messaging-sw.js'
+        const isRegistered = registrations.some(
+          (registration) =>
+            registration.active &&
+            registration.scope === "/firebase-messaging-sw.js"
         );
 
         if (!isRegistered) {
           try {
-            await navigator.serviceWorker.register('/firebase-messaging-sw.js');
-            console.log('Service Worker registered successfully');
+            await navigator.serviceWorker.register("/firebase-messaging-sw.js");
+            console.log("Service Worker registered successfully");
           } catch (err) {
-            console.error('Service Worker registration failed:', err);
+            console.error("Service Worker registration failed:", err);
           }
         } else {
-          console.log('Service Worker already registered');
+          console.log("Service Worker already registered");
         }
       }
     };
 
     registerServiceWorker();
   }, []);
-
 
   useEffect(() => {
     const requestPermissionsAndToken = async () => {
@@ -62,7 +57,7 @@ const CallBack = () => {
           const token = await requestForToken();
           if (token) {
             setToken(token);
-            // saveFcmToken(token, userId);
+            saveFcmToken(token, userId);
           } else {
             console.warn("No token received");
           }
@@ -72,25 +67,24 @@ const CallBack = () => {
       } else {
         console.warn("User ID is null, skipping requestPermissionsAndToken");
       }
-      
     };
-      requestPermissionsAndToken();
-    }, [setToken, userId]);
+    requestPermissionsAndToken();
+  }, [setToken, userId]);
 
   //== 회원 정보 ==//
-  const { data: userInfo } = useQuery('getUserInfo', getUserInfo, {
+  const { data: userInfo } = useQuery("getUserInfo", getUserInfo, {
     enabled: userInfoEnabled,
   });
 
   useEffect(() => {
     if (userInfo && userInfo[0]?.date != null) {
-      navigate('/');
+      navigate("/");
     } else if (userInfo) {
-      navigate('/userInfo');
+      navigate("/userInfo");
     }
   }, [userInfo, navigate]);
 
   return null;
-}
+};
 
 export default CallBack;
