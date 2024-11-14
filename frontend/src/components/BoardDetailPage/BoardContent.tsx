23e8b4ffb9c46876ce047cabbe5b59ee2d5a0925
이채연 <dldus0621@naver.com>
2024-11-14 10:24:08 +0900

    fix(token):fcm 토큰 저장 & css

--- frontend/src/components/BoardDetailPage/BoardContent.tsx
+++ frontend/src/components/BoardDetailPage/BoardContent.tsx
@@ -10,7 +10,7 @@ const BoardContent = ( { product }: BoardContentProp) => {
   const price = product?.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
   
   return (
-    <div className="mx-5 mb-24">
+    <div className="mx-5 mb-10">
       <div className="flex flex-col my-5">
         <span className="font-bold mt-2">{product?.name}</span>
         <span className="text-gray-500 text-sm">{product?.vendorName}</span>
--- frontend/src/pages/BoardDetailPage.tsx
+++ frontend/src/pages/BoardDetailPage.tsx
@@ -6,9 +6,13 @@ import { MainCarousel } from "../components/MainPage/MainCarousel";
 import { useParams } from "react-router-dom";
 import { detailProduct, getReviewList } from "@/api/productApi";
 import { addProductToCart } from "@/api/cartApi";
+import Separate from "@/common/Separate";
+import AlertBox from "@/common/AlertBox";
+import { useState } from "react";
 
 const BoardDetail = () => {
   const { productId } = useParams();
+  const [showAlert, setShowAlert] = useState(false);
 
   //== 상품 상세 데이터 ==//
   const { data: productDetail } = useQuery(
@@ -27,13 +31,20 @@ const BoardDetail = () => {
   //== 장바구니 담기 ==//
   const addToCart = async () => {
     await addProductToCart(productId);
+    setShowAlert(true); // 알림 상태를 true로 설정
+    setTimeout(() => setShowAlert(false), 2000); // 3초 후 알림 상태를 false로 변경
   };
 
   return (
     <div>
+      {showAlert && <AlertBox title="상품 담기" description="장바구니에 상품 담기 완료!"/>}
       <MainCarousel imageList={productDetail?.images} />
 
       <BoardContent product={productDetail} />
+      <div className="mx-5 mb-5">
+      <Separate />
+      </div>
+      <span className="m-5 font-bold">Review</span>
       <BoardReview reviewList={reviewList ?? []} />
 
       <div onClick={addToCart}>
