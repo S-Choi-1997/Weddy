8467104c84491e3055187376389869b7015ff521
이채연 <dldus0621@naver.com>
2024-11-14 14:01:27 +0900

    fix

--- frontend/src/api/authApi.ts
+++ frontend/src/api/authApi.ts
@@ -1,5 +1,4 @@
-const URL = process.env.VITE_PUBLIC_URL
-const BASE_URL = `${URL}/api/oauth2/authorization`
+const BASE_URL = "https://weddy.info/api/oauth2/authorization";
 //== 네이버 로그인 ==//
 export const naverLogin = () => {
   window.location.href = `${BASE_URL}/naver`;
