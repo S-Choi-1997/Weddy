f737f057b1d2588de40e35b1a463d48742c33cdd
이병수 <evil55@naver.com>
2024-11-17 17:21:38 +0900

    fix(schedule) : front 토큰을 봐보자

--- frontend/src/api/scheduleApi.ts
+++ frontend/src/api/scheduleApi.ts
@@ -1,18 +1,19 @@
 import axios from "axios";
 import { GetSchedule, Schedule } from "./schedule.type";
 
-const BASE_URL = `/api/schedules`
+const BASE_URL = `/api/schedules`;
 
 //== 일정 등록 ==//
 export const schedule = async (scheduleData?: Schedule): Promise<void> => {
   console.log(scheduleData);
+  console.log(sessionStorage.getItem("token"));
   await axios({
     method: "post",
     url: BASE_URL,
     headers: {
       Authorization: sessionStorage.getItem("token"),
     },
-    data: scheduleData
+    data: scheduleData,
   });
 };
 
@@ -25,8 +26,8 @@ export const getSchedule = async (selectedDate: string): Promise<GetSchedule[]> 
       Authorization: sessionStorage.getItem("token"),
     },
     params: {
-      date: selectedDate
-    }
+      date: selectedDate,
+    },
   });
   return response.data.data;
 };
