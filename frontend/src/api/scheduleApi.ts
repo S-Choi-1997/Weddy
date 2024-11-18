c0114f658d18661f35fa823cbecfa6a867881c44
이채연 <dldus0621@naver.com>
2024-11-18 13:29:37 +0900

    fix(nft)

--- frontend/src/api/scheduleApi.ts
+++ frontend/src/api/scheduleApi.ts
@@ -5,8 +5,6 @@ const BASE_URL = `/api/schedules`;
 
 //== 일정 등록 ==//
 export const schedule = async (scheduleData?: Schedule): Promise<void> => {
-  console.log(scheduleData);
-  console.log(sessionStorage.getItem("token"));
   await axios({
     method: "post",
     url: BASE_URL,
